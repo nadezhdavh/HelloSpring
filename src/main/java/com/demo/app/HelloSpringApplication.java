fcde4dd2f7e065dfa520b6e6b78bb9eae7174f73
Nadezhda <nadezhdavhristeva@gmail.com>
2024-06-26 18:07:35 +0300

    Add logging

--- src/main/java/com/demo/app/HelloSpringApplication.java
+++ src/main/java/com/demo/app/HelloSpringApplication.java
@@ -1,5 +1,7 @@
 package com.demo.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -7,7 +9,10 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 public class HelloSpringApplication {
+	private static final Logger logger = LoggerFactory.getLogger(HelloSpringApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
+		logger.info("Start");
 	}
 }
